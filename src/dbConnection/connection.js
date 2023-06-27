fec5f5ff9f4d8f2a47dd7f269fc48097437be155
Gary.Morga <sergio.morga@impuestos.gob.bo>
2023-06-27 17:47:45 -0400

    Se consumio datos de la base de datos de MongoDb con exito

--- src/dbConnection/connection.js
+++ src/dbConnection/connection.js
@@ -3,16 +3,48 @@
     * @description  Configuración y conexion a la base de datos en Mongo
 */
 const mongoose = require('mongoose')
-
 const password ="dsin";
 const dbname = "dsin";
-          // mongodb://myUser:userPW@SG-staging-111.servers.mongodirector.com:27017,SG-staging-43334.servers.mongodirector.com:27017/stagingDB?replicaSet=RS-staging-0&ssl=true&authSource=stagingDB
-const uri = `mongodb://dsin:${password}@dcbdmongo.impuestos.gob.bo:27017/?authMechanism=DEFAULT&authSource=${dbname}`;   
+const uri = `mongodb://dsin:${password}@dcbdmongo.impuestos.gob.bo:27017/${dbname}?authMechanism=DEFAULT&authSource=${dbname}retryWrites=true&w=majority`;   
 
+module.exports = ()=>  mongoose.connect(uri, 
+    {maxPoolSize: 50, 
+     wtimeoutMS: 2500,
+     authSource:"dsin",
+     user:"dsin",
+     pass:"dsin",
+     useNewUrlParser:true
+    }).then(
+        ()=>{
+           console.log("La conexion de la base de datos se conecto correctamente")     
+        },
+        (err) => {
+            console.log("connection error", err);
+        }
+    )
+    
+//const uri_2 = "mongodb://dcbdmongo.impuestos.gob.bo:27017/"
 /*const connection = ()=>{
     mongoose.connect(uri, {userNewUrlParser:true, useUnifiedTopology:true});
 }
 // module.exports = connection
 */
-
-module.exports = ()=>  mongoose.connect(uri, {maxPoolSize: 50, wtimeoutMS: 2500});
+// OPCION 2 Prueba de conexion de base de datos.
+/*module.exports = ()=> mongoose.connect(uri_2, {
+            // poolSize: 10,
+            authSource: "dsin",
+            user: "dsin",
+            pass: "dsin", 
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            authMechanism: "DEFAULT",
+            //useCreateIndex: true,
+            //useFindAndModify: false 
+}).then(
+    ()=> {
+        console.log("coneccion de ready to use");
+    },
+    (err) => {
+        console.log("connection error - ", err)
+    }
+)*/
--- src/models/notificacion_push.js
+++ src/models/notificacion_push.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
-// Modelo de base de datos Prueba Mongo 
+// Modelo de base de datos Prueba
 const NotificacionesPushSchema = new Schema({
     ci: String,
     mac: String
-})
+});
 
 const NotificacionesPushModel = mongoose.model('sad_not_notificaciones_push_pruebas', NotificacionesPushSchema);
 
-module.exports = NotificacionesPushModel;
+module.exports = NotificacionesPushModel;   
