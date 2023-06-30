189d3b38c917b6d232c9a94fc005337204a4c0e1
Gary.Morga <sergio.morga@impuestos.gob.bo>
2023-06-30 17:49:05 -0400

    Se creo el modelo de sad_not_notificaciones para obtener los datos de MongoDB al servidor de NodeJs viqui.alaro/33-servidor-movil-notificacion#1

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,8 @@
 const controller = {}
 const connection = require('../dbConnection/connection')
 const NotificacionesPushModel = require('../models/notificacion_push')
+const SadNotNotificacionesModel = require('../models/sad_not_notificaciones')
+
 
 controller.index =  async (req, res)=>{
     try{
@@ -10,23 +12,43 @@ controller.index =  async (req, res)=>{
         console.log("Conexionde base de Datos");
 
         const allNotificacionesPush = await NotificacionesPushModel.find();
-
+        const sanNotNotificacionesPushFinal = await SadNotNotificacionesModel.find();
         console.log(allNotificacionesPush);
+        console.log("--------------- Obtencionde las notificaciones --------");
+        console.log(sanNotNotificacionesPushFinal);
     }catch(err){
         console.log(err);
     }
 
 }
 
-// TODO Prueba 
+/**
+    @author gary_morga
+    @description Metodo para la obtencion de los datos de la colección de datos sad_notificaciones_push_prueba
+    @date 30/06/2023
+**/
 
 async function obtenerDatos(){
     const allNotificacionesPush = await NotificacionesPushModel.find();
     console.log(allNotificacionesPush);
 }
 
+/**
+ * @author gary_morga
+ * @description 
+ */
+
+async function obtenerDatosNotificacion(){
+    const sanNotNotificacionesPushFinal = await SadNotNotificacionesModel.find();
+    console.log("------------------------------ Obtencion de las notificaciones ");
+    console.log(sanNotNotificacionesPushFinal);
+}
 module.exports = controller;
 module.exports = obtenerDatos;
+module.exports = obtenerDatosNotificacion
+
+
+
 
 //Ejemplo Sencillo
 /*controller.name = 'Gary'
