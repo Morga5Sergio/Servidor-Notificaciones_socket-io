b4ba66f4a1943d234a57eb4298718fb372c6b9ab
codefher <codefher@gmail.com>
2023-11-09 10:10:25 -0400

    update shared and uuid unique v2

--- src/util/uuid.util.js
+++ src/util/uuid.util.js
@@ -1,6 +1,6 @@
 // Generar un número aleatorio único y convertirlo a una cadena sin el punto
 const uniqueRandomNumber = (Math.random() + Date.now()).toString().replace('.', '')
 // const uniqueRandomNumberWithText = 'UniqueRandom_' + uniqueRandomNumber  // * Llave generada
-const uniqueRandomNumberWithText = 'UniqueUnique'
+const uniqueRandomNumberWithText = 'Notificacion'
 
 export default uniqueRandomNumberWithText
